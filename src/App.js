2ef4452919d3a04614c2c0d1d1927cf90ed2e4d0
Kim Brannian <brannian@gmail.com>
2022-03-18 14:26:13 -0700

    add cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import SingleCard from './components/SingleCard'
 import './App.css'
 
 
@@ -15,19 +16,38 @@ const cardImages = [
 function App() {
 const [ cards, setCards ] = useState([])
 const [ turns, setTurns ] = useState(0)
+const [choiceOne, setChoiceOne] = useState(null)
+const [choiceTwo, setChoiceTwo] = useState(null)
 
   //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
       .sort(()=> Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffledCards)
+    setTurns(0)
   }
 
+const handleChoice =(card)=>{
+  choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+}
+ 
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card=> (
+          <SingleCard 
+          key={card.id}
+          card={card}
+          handleChoice={handleChoice} 
+          /> 
+        ))}
+      </div>
     </div>
   );
 }
